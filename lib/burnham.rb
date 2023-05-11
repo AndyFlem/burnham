3e5f9215d3c5da81b78370ffd6e45de3443a5492
Andy Fleming <andy@ulendo.com>
2023-05-11 09:43:47 +0200

    Update

---
+++ lib/burnham.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "burnham/version"
+
+module Burnham
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/burnham/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Burnham
+  VERSION = "0.1.0"
+end

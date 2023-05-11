8387368681ca53697a6c560fc3c4da7d843815fc
Andy Fleming <andy@ulendo.com>
2023-05-11 09:43:55 +0200

    Update

--- lib/burnham.rb
+++ lib/burnham.rb
@@ -4,5 +4,26 @@ require_relative "burnham/version"
 
 module Burnham
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Model
+    @frames = Hash.new
+
+    def add_frame(frame)
+      @frames[frame.ref] = frame
+    end
+  end
+
+  class Frame
+    @name = ''
+    @model = nil
+    @ref = nil
+
+    def initialize(params)
+      @name=params[:name]
+      @model = params[:model]
+      @ref = params[:ref]
+      model.add_frame(self)
+    end
+
+  end
 end
